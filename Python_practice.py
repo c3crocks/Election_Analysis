ac050d4ffeaf3787b392c53cae0fb1894594ed69
Chirayu Chaudhari <chirayuchaudhari1990@gmail.com>
2021-08-29 20:51:09 -0400

    Adding script

--- Python_practice.py
+++ Python_practice.py
@@ -1,81 +1,17 @@
-# # # # counties = ["Arapahoe","Denver","Jefferson"]
-# # # # # counties.insert(2,"Lucknow")
-# # # # # counties.insert(counties.index("Arapahoe"),"Bangalore")
-# # # # # #counties.append("El Paso")
-# # # # # #counties.remove("El Paso")
-# # # # # #counties[2]="Mumbai"
-# # # # # y=len(counties)
-# # # # # print(y)
-# # # # # print((counties[:y]))
+# # Import the datetime class from the datetime module.
+# import datetime as dt
+# # Use the now() attribute on the datetime class to get the present time.
+# now = dt.datetime.now()
+# # Print the present time.
+# print("The time right now is ", now)
 
-# # # # # counties_dict = {}
-# # # # # counties_dict["Araphoe"] = 422829
-# # # # # counties_dict["Denver"] = 546558
-# # # # # counties_dict["Jefferson"] = 462168
+# Assign a variable for the file to load and the path.
+file_to_load = 'Resources/election_results.csv'
 
+# Open the election results and read the file.
+election_data = open(file_to_load, 'r')
 
+# To do: perform analysis.
 
-# # # # # print(counties_dict["Jefferson"])
-# # # # # print(counties_dict.items())
-
-# # # # voting_data = []
-# # # # voting_data.append ({"county": "Arapahoe", "registered_voters": "422829"})
-# # # # voting_data.append ({"county": "Denver", "registered_voters":"463353"})
-# # # # voting_data.append ({"county": "Jefferson", "registered_voters":"432438"})
-# # # # voting_data.insert(2,{"county": "El Paso", "registered_voters": "461149"})
-# # # # voting_data.remove({"county": "Arapahoe", "registered_voters": "422829"})
-# # # # voting_data.remove({"county": "Denver", "registered_voters": "463353"})
-# # # # voting_data.insert(3,{"county": "Denver", 70"registered_voters": "463353"})
-# # # # voting_data.append({"county": "Arapahoe", "registered_voters": "422829"})
-
-# # # # # print(voting_data)
-
-# counties = ["El Paso","Arapahoe","Denver","Jefferson"]
-# # # # if "El Paso" in counties:
-
-# # # #    print(counties[1])
-# # # # else:
-# # # #     print(counties[0])
-
-# # # # temperature = int(input("What is the temperature outside? "))
-
-# # # # if temperature > 80:
-# # # #     print("Turn on the AC.")
-# # # # else:
-# # # #     print("Open the windows.")
-
-# # # # #What is the score?
-# # # # score = int(input("What is the score? "))
-
-# # # # #Determine the Grade
-# # # # if score >= 90:
-# # # #     print("Your grade is A")
-# # # # elif score >=80:
-# # # #     print("Your grade is B")
-# # # # elif score >=70:
-# # # #     print("Your grade is C")
-# # # # elif score >=60:
-# # # #     print("Your grade is D")
-# # # # else:
-# # # #     print("Your grade is F")
-
-# # # if "Arapahoe" in counties and "El Paso" not in counties:
-# # #    print("Only Arapahoe is in the list of counties.")
-# # # else:
-# # #     print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
-
-
-# # for county in counties:
-# #     print(county)
-
-# # for num in range(7):
-# #     print(num)
-
-# for i in range(len(counties)):
-#     print(counties[i])
-
-voting_data = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-
-for county_dict in voting_data:
-    for value in voting_data.values():
-        print(value)
+# Close the file.
+election_data.close()
